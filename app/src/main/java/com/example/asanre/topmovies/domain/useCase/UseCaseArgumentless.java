4abe0f8fd57d8e57449219878629437daf17a00a
asanre <aser04@gmail.com>
2017-11-23 02:27:56 +0100

    Remove use case argumentless

--- app/src/main/java/com/example/asanre/topmovies/domain/useCase/UseCaseArgumentless.java
+++
@@ -1,8 +0,0 @@
-package com.example.asanre.topmovies.domain.useCase;
-
-import com.example.asanre.topmovies.data.network.callbacks.ServiceCallback;
-
-public interface UseCaseArgumentless<T> {
-
-    void execute(ServiceCallback<T> callback);
-}
