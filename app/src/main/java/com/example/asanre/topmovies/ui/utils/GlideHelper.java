812b14c10e61d411f13f3a2be3069fcd40c184fe
asanre <aser04@gmail.com>
2017-11-24 01:10:33 +0100

    Set base url according to network

--- app/src/main/java/com/example/asanre/topmovies/ui/utils/GlideHelper.java
+++ app/src/main/java/com/example/asanre/topmovies/ui/utils/GlideHelper.java
@@ -2,6 +2,8 @@ package com.example.asanre.topmovies.ui.utils;
 
 import android.content.Context;
 import android.graphics.drawable.Drawable;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
@@ -17,15 +19,18 @@ public class GlideHelper {
 
     public static void getImageForList(ImageView imageView, Context context, String url) {
 
+        setBaseUrls(context);
         Glide.with(context).load(getListImageUrl(url))
                 .apply(new RequestOptions().error(R.drawable.ic_image_error)
-                        .placeholder(R.drawable.ic_image_holder).override(110, 130)
+                        .placeholder(R.drawable.ic_image_holder)
+                        .override(110, 130)
                         .centerCrop())
                 .into(imageView);
     }
 
     public static void getHeroImage(ImageView imageView, Context context, String url) {
 
+        setBaseUrls(context);
         RequestBuilder<Drawable> thumbnail = Glide.with(context)
                 .load(getListImageUrl(url))
                 .apply(new RequestOptions().dontAnimate().centerCrop().priority(Priority.HIGH));
@@ -37,6 +42,31 @@ public class GlideHelper {
                         .centerCrop())
                 .thumbnail(thumbnail)
                 .into(imageView);
+    }
+
+    private static void setBaseUrls(Context context) {
+
+        if (deviceOnWifi(context)) {
+            baseLowUrl = "http://image.tmdb.org/t/p/w185%s";
+            baseHighUrl = "http://image.tmdb.org/t/p/original%s";
+        } else {
+            baseLowUrl = "http://image.tmdb.org/t/p/w92%s";
+            baseHighUrl = "http://image.tmdb.org/t/p/w500%s";
+        }
+    }
+
+    private static boolean deviceOnWifi(Context context) {
+
+        ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(
+                Context.CONNECTIVITY_SERVICE);
+        if (connectivityManager != null) {
+
+            NetworkInfo networkInfo = connectivityManager.getNetworkInfo(
+                    ConnectivityManager.TYPE_WIFI);
+            return networkInfo.isConnected();
+        }
+
+        return false;
     }
 
     private static String getListImageUrl(String path) {
